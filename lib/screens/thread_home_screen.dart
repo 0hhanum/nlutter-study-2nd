cbde27ad59a19de527ee6839f87fa474827127f4
hanum <rntls123@naver.com>
2023-12-27 23:35:48 +0900

    feat: infinite scroll on post timeline

--- lib/screens/thread_home_screen.dart
+++ lib/screens/thread_home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:challenge/constants/sizes.dart';
-import 'package:challenge/data/post_data.dart';
 import 'package:challenge/navs/thread_main_nav.dart';
 import 'package:challenge/posts/view_models/timeline_vm.dart';
 import 'package:challenge/utils/utils.dart';
@@ -7,11 +6,46 @@ import 'package:challenge/widgets/home_screens/thread_post_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ThreadHomeScreen extends ConsumerWidget {
+class ThreadHomeScreen extends ConsumerStatefulWidget {
   const ThreadHomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ThreadHomeScreenState createState() => ThreadHomeScreenState();
+}
+
+class ThreadHomeScreenState extends ConsumerState<ThreadHomeScreen> {
+  final ScrollController _scrollController = ScrollController();
+  bool isLoading = false;
+
+  void _scrollListener() async {
+    if (_scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent &&
+        !isLoading) {
+      setState(() {
+        isLoading = true;
+      });
+      await ref.read(timelineProvider.notifier).fetchNextPosts();
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final isDark = isDarkMode(context);
     return ref.watch(timelineProvider).when(
           data: (posts) => Scaffold(
@@ -24,17 +58,33 @@ class ThreadHomeScreen extends ConsumerWidget {
                 ),
               ),
             ),
-            body: ListView.separated(
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size14,
-              ),
-              itemCount: posts.length,
-              separatorBuilder: (context, index) => const Divider(),
-              itemBuilder: (context, index) {
-                return ThreadPostCard(
-                  post: posts[index],
-                );
-              },
+            body: Stack(
+              children: [
+                ListView.separated(
+                  controller: _scrollController,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: Sizes.size14,
+                  ),
+                  itemCount: posts.length,
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemBuilder: (context, index) {
+                    return ThreadPostCard(
+                      post: posts[index],
+                    );
+                  },
+                ),
+                if (isLoading)
+                  Positioned(
+                    width: MediaQuery.of(context).size.width,
+                    bottom: Sizes.size10,
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircularProgressIndicator.adaptive(),
+                      ],
+                    ),
+                  ),
+              ],
             ),
           ),
           error: (error, stackTrace) => Center(
